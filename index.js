b52190c13ee8858395ec60e1b39126d5530631b0
Vanessa <yu100997@gmail.com>
2015-01-16 11:13:56 -0500

    changed ideas, got it to show current position and opponent position which is fake right now

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var myPos;
 var map;
-
+/*
 function initialize(){
     var mapOptions = {
 	zoom: 15	
@@ -25,12 +25,74 @@ function initialize(){
 
 var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
 
-// To add the marker to the map, use the 'map' property
+
 var marker = new google.maps.Marker({
     position: myLatlng,
-    title:"Hello World!"
+    title:"Hello World!",
+    map: map,
+    visible: true
+});
+console.log(marker.position);
+*////////
+/*
+var markerme;
+function initialize() {
+    latLng = new google.maps.LatLng(-8.064903, -34.896872)
+    var mapOptions = {
+	center: latLng,
+	zoom: 16,
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    
+    markerme = new google.maps.Marker({
+	position: latLng,
+	title:"Hello World!",
+	visible: true,
+	map: map
+    });
+}
+
+var markerset = function markerset(marker){
+    console.log("markers????");
+    var new_marker_position = new google.maps.LatLng(marker.position.lat()+5, marker.position.lng+5);
+    marker.setPosition(new_marker_position);
+    console.log(marker.position.lat());
+}
+
+var meset = function(){
+    markerset(markerme);
+    markerme.visible = true;
+}
+
+google.maps.event.addDomListener(window, 'load', initialize());
+window.setInterval(meset, 3000);
+
+*/
+var marker;
+$(window).load(function() {
+    myOptions = { 
+	zoom: 20,
+	mapTypeId: google.maps.MapTypeId.ROADMAP 
+    };
+    map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+    var watchId = navigator.geolocation.watchPosition(centerMap); 
+    var myLatlng = new google.maps.LatLng(location.coords.latitude,location.coords.longitude);
+    marker = new google.maps.Marker({
+	position: myLatlng,
+	map: map,
+	zIndex:1
+    });
 });
 
-marker.setMap(map);
-
-google.maps.event.addDomListener(window, 'load', initialize);
+function centerMap(location)
+{
+    var myLatlng = new google.maps.LatLng(location.coords.latitude,location.coords.longitude);
+    map.setCenter(myLatlng);
+    map.setZoom(15);
+    
+    $("#lat").text("Latitude : " + location.coords.latitude);
+    $("#lon").text("Longitude : " + location.coords.longitude);
+ 
+   //show current location on map
+    marker.setPosition(myLatlng);
+}
