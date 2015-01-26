4eb82e4e4cabc539aedc891178347f4f51d60717
jasonluX13 <jasonluX13@gmail.com>
2015-01-25 19:57:38 -0500

    Added variables for user location and ajax

--- index.js
+++ index.js
@@ -1,98 +1,82 @@
-var myPos;
-var map;
-/*
-function initialize(){
-    var mapOptions = {
-	zoom: 15	
+var initialLocation;
+var siberia = new google.maps.LatLng(60, 105);
+var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+var browserSupportFlag =  new Boolean();
+
+var targetlat = 40;
+var targetlng = -70;
+var mylat = 40;
+var mylng = -70;
+
+var init = function initialize() {
+    var myOptions = {
+        zoom: 6,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-				  mapOptions);
+    var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+
     if(navigator.geolocation) {
-	browserSupportFlag = true;
-	navigator.geolocation.getCurrentPosition(function(position) {
-	    myPos = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-	    map.setCenter(myPos);
-	}, function() {
-	    handleNoGeolocation(browserSupportFlag);
-	});
-    }
+        browserSupportFlag = true;
+        navigator.geolocation.getCurrentPosition(function(position) {
+            initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+            mylat = position.coords.latitude;
+            mylng = position.coords.longitude;
+            map.setCenter(initialLocation);
 
+            var myLatlng = initialLocation;
+            var myMarker = new google.maps.Marker({
+                position: myLatlng,
+                map: map,
+                title:"Hello World!"
+            });
+
+            var targetLatLng = new google.maps.LatLng(targetlat,targetlng);
+            var targetMarker = new google.maps.Marker({
+                position: targetLatLng,
+                map: map,
+                title:"Hello World!"
+            });
+
+        }, function() {
+            handleNoGeolocation(browserSupportFlag);
+        });
+    }
     else {
-	browserSupportFlag = false;
-	handleNoGeolocation(browserSupportFlag);
+        browserSupportFlag = false;
+        handleNoGeolocation(browserSupportFlag);
+    }
+
+    function handleNoGeolocation(errorFlag) {
+        if (errorFlag == true) {
+            alert("Geolocation service failed.");
+            initialLocation = newyork;
+        } else {
+            alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+            initialLocation = siberia;
+        }
+        map.setCenter(initialLocation);
     }
 }
-
-var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
-
-
-var marker = new google.maps.Marker({
-    position: myLatlng,
-    title:"Hello World!",
-    map: map,
-    visible: true
-});
-console.log(marker.position);
-*////////
-/*
-var markerme;
-function initialize() {
-    latLng = new google.maps.LatLng(-8.064903, -34.896872)
-    var mapOptions = {
-	center: latLng,
-	zoom: 16,
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-    
-    markerme = new google.maps.Marker({
-	position: latLng,
-	title:"Hello World!",
-	visible: true,
-	map: map
+var log = function log() {
+    $.ajax(
+    {
+      type: "POST",
+      url: "app.py",
+      data: "stuff_for_python=" + mylat + mylng,
+      success: function(response)
+        {
+          console.log("success");
+        },
+      error: function(response)
+        {
+          console.log("error");
+        }
     });
 }
-
-var markerset = function markerset(marker){
-    console.log("markers????");
-    var new_marker_position = new google.maps.LatLng(marker.position.lat()+5, marker.position.lng+5);
-    marker.setPosition(new_marker_position);
-    console.log(marker.position.lat());
+function setTarget(lat,lng){
+    targetlat = lat;
+    targetlng= lng;
+    init();
 }
 
-var meset = function(){
-    markerset(markerme);
-    markerme.visible = true;
-}
-
-google.maps.event.addDomListener(window, 'load', initialize());
-window.setInterval(meset, 3000);
-
-*/
-var marker;
-$(window).load(function() {
-    myOptions = { 
-	zoom: 20,
-	mapTypeId: google.maps.MapTypeId.ROADMAP 
-    };
-    map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-    var watchId = navigator.geolocation.watchPosition(centerMap); 
-    var myLatlng = new google.maps.LatLng(location.coords.latitude,location.coords.longitude);
-    marker = new google.maps.Marker({
-	position: myLatlng,
-	map: map,
-	zIndex:1
-    });
-});
-
-function centerMap(location)
-{
-    var myLatlng = new google.maps.LatLng(location.coords.latitude,location.coords.longitude);
-    map.setCenter(myLatlng);
-    map.setZoom(15);
-    
-    $("#lat").text("Latitude : " + location.coords.latitude);
-    $("#lon").text("Longitude : " + location.coords.longitude);
- 
-   //show current location on map
-    marker.setPosition(myLatlng);
-}
+window.onLoad = init();
