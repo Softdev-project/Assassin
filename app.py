c1184462f4d655fb6486594fcb50a91fd9f88e07
jasonluX13 <jasonluX13@gmail.com>
2015-01-26 22:24:04 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, jsonify
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+
+# This route will show a form to perform an AJAX request
+# jQuery is loaded to execute the request and update the
+# value of the operation
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Route that will process the AJAX request, sum up two
+# integer numbers (defaulted to zero) and return the
+# result as a proper JSON response (Content-Type, etc.)
+@app.route('/_add_numbers')
+def add_numbers():
+    mylat = request.args.get('mylat', 0, type=int)
+    mylng = request.args.get('mylng', 0, type=int)
+    return jsonify(result=mylat + mylng)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = "127.0.0.1", port = 8000)
